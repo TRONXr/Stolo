42831f40fce7a1d915c4f3612671c3d9199c2227
john.gerick.86 <john.gerick.86@gmail.com>
2024-09-18 15:09:52 +0500

    ссделала гуйляй

---
+++ STOLOVA/Pages/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STOLOVA.Pages
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = "Заказ успешно добавлен!";
+                return RedirectToAction("Index"); // Поменяйте на нужное действие после создания
+            }
+
+            return View(order);
+        }
+
+    }
+}
